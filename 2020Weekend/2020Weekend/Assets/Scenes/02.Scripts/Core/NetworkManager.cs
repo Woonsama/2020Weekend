54c5702eac2e5680d73bd235183d4634f631e1d9
BeomGeun Choi <35332637+ChoiBeomGeun@users.noreply.github.com>
2020-12-12 03:55:25 +0900

    이미지 에셋 추가 타이틀 추가 및 로비 추가

---
+++ 2020Weekend/2020Weekend/Assets/Scenes/02.Scripts/Core/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    TMP_InputField _nicknameField;
+
+    private void Awake()
+    {
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = _nicknameField.text;
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 8 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Lobby");
+    }
+}
---
+++ 2020Weekend/2020Weekend/Assets/Scenes/02.Scripts/View/TitleView2D.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleView2D : MonoBehaviour
+{
+
+
+    public void OnClickStartBtn()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Lobby");
+    }
+}
