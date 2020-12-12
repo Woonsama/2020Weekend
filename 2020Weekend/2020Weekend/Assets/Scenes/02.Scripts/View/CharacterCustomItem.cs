65d97f3532cacfabdb75d09a07a831ea20e0f275
BeomGeun Choi <35332637+ChoiBeomGeun@users.noreply.github.com>
2020-12-13 00:04:14 +0900

    선택한 ACC 강조하는 기능 추가

--- 2020Weekend/2020Weekend/Assets/Scenes/02.Scripts/View/CharacterCustomItem.cs
+++ 2020Weekend/2020Weekend/Assets/Scenes/02.Scripts/View/CharacterCustomItem.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CharacterCustomItem : MonoBehaviour
 {
+    Color _highlightColor = new Color(255, 255, 0);
+    Color _normalColor = new Color(255, 255, 255);
+
     [SerializeField]
     UnityEngine.UI.Image _image;
 
@@ -17,12 +20,12 @@ public class CharacterCustomItem : MonoBehaviour
         public int index;
         public LobbyCharacter.AccessorieType accType;
         public string itemName;
-        public System.Action<LobbyCharacter.AccessorieType, string> onClick;
+        public System.Action<LobbyCharacter.AccessorieType, string,int> onClick;
     }
 
     UIData _uiData;
 
-    System.Action<LobbyCharacter.AccessorieType, string> _onClickAction;
+    System.Action<LobbyCharacter.AccessorieType, string,int> _onClickAction;
 
     public void Init(UIData data)
     {
@@ -36,8 +39,18 @@ public class CharacterCustomItem : MonoBehaviour
     {
         if(_onClickAction != null)
         {
-            _onClickAction(_uiData.accType, _uiData.prefabName);
+            _onClickAction(_uiData.accType, _uiData.prefabName,_uiData.index);
         }
+    }
+
+    public void HighlightItem()
+    {
+        _image.color = _highlightColor;
+    }
+
+    public void DisHighlightItem()
+    {
+        _image.color = _normalColor;
     }
 
 }
