975d4d8811032a8c3bc7ba3678d0cd2e7adc3436
jadeshine22 <jadeshine22@gmail.com>
2020-12-12 20:08:48 +0900

    [아트][김민욱]레벨 2 관련 리소스 업로드

---
+++ 2020Weekend/2020Weekend/Assets/Scenes/02.Scripts/KWS/Hammer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour
+{
+    [Header("Start Time")]
+    public float startTime;
+
+    [Header("Animation Speed")]
+    public float animationSpeed = 1;
+
+    private Animation anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animation>();
+        Invoke("PlayAnim", startTime);
+    }
+
+    private void PlayAnim()
+    {
+        anim["anim_hammer"].speed = animationSpeed;
+        anim.Play("anim_hammer");
+    }
+
+}
