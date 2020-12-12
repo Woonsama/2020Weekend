60105046e53921d5dd5828db9f6d01f145a515c2
kyungookpark <68501510+kyungookpark@users.noreply.github.com>
2020-12-12 16:11:45 +0900

    Spinning Obstacle

---
+++ 2020Weekend/2020Weekend/Assets/Scenes/02.Scripts/Etc/SpinningObstacle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningObstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float maxVel = 100.0f;
+    private float minVel = 50.0f;
+    private float randVel;
+    
+    
+
+    HingeJoint joint;
+    JointMotor motor;
+
+    void SetMotorVelocity(float vel)
+    {
+        motor.targetVelocity = vel;
+    }
+
+    void SetjointMotor()
+    {
+        joint.motor = motor;
+    }
+
+    void Start()
+    {
+        joint = GetComponent<HingeJoint>();
+        randVel = Random.Range(maxVel, minVel);
+        motor.force = 10000.0f;
+        SetMotorVelocity(randVel);
+        SetjointMotor();
+        StartCoroutine(Test(Random.Range(8.0f, 15.0f)));
+    }
+
+    // Update is called once per frame
+    IEnumerator Test(float waitTime)
+    {
+        Debug.Log("Courtine");
+        yield return new WaitForSeconds(waitTime);
+        SetMotorVelocity(-randVel);
+        SetjointMotor();
+        StartCoroutine(Test(Random.Range(8.0f, 15.0f)));
+    }
+}
