c93c82ae7387a35beb34aa46816f62d8f2048cdf
BeomGeun Choi <35332637+ChoiBeomGeun@users.noreply.github.com>
2020-12-12 11:25:51 +0900

    커스텀마이징 씬 추가
    모델 프리펩 위치 수정 (Assets/Art/Prefab -> Asset/Resources/Prefab)

--- 2020Weekend/2020Weekend/Assets/Scenes/02.Scripts/View/LobbyView2D.cs
+++ 2020Weekend/2020Weekend/Assets/Scenes/02.Scripts/View/LobbyView2D.cs
@@ -6,5 +6,29 @@ using Photon.Pun;
 
 public class LobbyView2D : MonoBehaviour 
 {
+    [SerializeField]
+    GameObject _characterRoot;
 
+
+    public void CreateCharacter(string prefabName)
+    {
+        foreach(Transform transform in _characterRoot.transform)
+        {
+            Destroy(transform.gameObject);
+        }
+
+        GameObject loadObj = Resources.Load("Prefab/" + prefabName) as GameObject;
+        GameObject gameObj = GameObject.Instantiate(loadObj);
+      
+        gameObj.transform.SetParent(_characterRoot.transform);
+        gameObj.transform.localScale = Vector3.one;
+        gameObj.transform.localPosition = Vector3.zero;
+        gameObj.transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+
+    private void Update()
+    {
+      //  if(Input.GetKeyDown("escape"))
+         //   UnityEngine.SceneManagement.LoadSCe
+    }
 }
